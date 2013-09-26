a45e044d9cd27a319b095d63e15479fa11f7bd58
Mickael Istria <mistria@redhat.com>
2013-09-26 15:47:45 +0200

    Ignore Drag and Drop tests when using Xvnc
    Change-Id: I2969da96786864beb331b968664148c2d158b2c5

--- org.eclipse.swtbot.swt.finder.test/src/org/eclipse/swtbot/swt/finder/widgets/DnDTreeTest.java
+++ org.eclipse.swtbot.swt.finder.test/src/org/eclipse/swtbot/swt/finder/widgets/DnDTreeTest.java
@@ -1,12 +1,12 @@
 package org.eclipse.swtbot.swt.finder.widgets;
 
-import static org.eclipse.swtbot.swt.finder.SWTBotAssert.assertDoesNotContain;
-import static org.junit.Assert.assertTrue;
+import java.io.IOException;
 
 import org.eclipse.swt.examples.dnd.DNDExample;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swtbot.swt.finder.test.AbstractSWTShellTest;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 
 public class DnDTreeTest extends AbstractSWTShellTest {
@@ -16,8 +16,10 @@ public class DnDTreeTest extends AbstractSWTShellTest {
 		// TODO Auto-generated method stub
 		new DNDExample().open(shell);
 	}
+
 	@Test
 	public void dragsAndDropsFromTreeToTree() throws Exception {
+		Assume.assumeFalse("Drag and drop does not work with Xvnc", isUsingXvnc());
 		bot.comboBoxInGroup("Widget", 0).setSelection("Tree");
 		bot.comboBoxInGroup("Widget", 1).setSelection("Tree");
 		final SWTBotTree sourceTree = bot.tree(0);
@@ -36,7 +38,22 @@ public class DnDTreeTest extends AbstractSWTShellTest {
 		Assert.assertNotNull("Could not find dropped node", droppedNote);
 	}
 
-
+	public static boolean isUsingXvnc() throws Exception {
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.indexOf("nix") < 0 && os.indexOf("nux") < 0 && os.indexOf("aix") < 0) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			return false;
+		}
+		String xdisplay = System.getenv("DISPLAY"); //$NON-NLS-1$
+		// command is like    pgrep -l -f "Xvnc.*:0"
+		StringBuilder commandBuilder = new StringBuilder();
+		commandBuilder.append("/usr/bin/pgrep -l -f "); //$NON-NLS-1$
+		commandBuilder.append("X.*"); //$NON-NLS-1$
+		commandBuilder.append(xdisplay);
+		Process proc = Runtime.getRuntime().exec(commandBuilder.toString());
+		proc.waitFor();
+		// If pgrep found something, it will return 0;
+		return proc.exitValue() == 0;
+	}
 
 
 }

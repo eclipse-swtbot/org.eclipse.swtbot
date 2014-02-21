374bfc51c1a384a496f0b085a80f142c155b267d
Mickael Istria <mistria@redhat.com>
2014-02-21 16:33:05 +0100

    [428738] Contribution of rules for Workbench
    * Added an extension point to add rules from other
    bunldes
    * Added rules for editor/view show()/close()
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=428738
    Signed-off-by: Mickael Istria <mistria@redhat.com>
    Change-Id: I4ebabf2c4ff37775f725890e34ab6241c9503c97

---
+++ org.eclipse.swtbot.generator.rules.workbench/src/org/eclipse/swtbot/generator/rules/workbench/CloseEditorSimpleRule.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Red Hat Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mickael Istria (Red Hat) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtbot.generator.rules.workbench;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.swtbot.generator.framework.GenerationSimpleRule;
+import org.eclipse.ui.IEditorReference;
+
+public class CloseEditorSimpleRule extends GenerationSimpleRule {
+
+	private IEditorReference editor;
+	public String widgetAccessor;
+
+	@Override
+	public boolean appliesTo(Event event) {
+		return event.widget == null && event.type == SWT.Selection && event.data instanceof IEditorReference && event.detail == 0; 
+	}
+
+	@Override
+	public void initializeForEvent(Event event) {
+		this.editor = (IEditorReference) event.data;
+		this.widgetAccessor = WorkbenchItemsUtil.getWidgetAccessor(this.editor);
+	}
+
+	@Override
+	public Widget getWidget() {
+		return null;
+	}
+
+	@Override
+	public List<String> getActions() {
+		return Arrays.asList(new String[] { this.widgetAccessor + ".close()" });
+	}
+
+	@Override
+	public List<String> getImports() {
+		return null;
+	}
+
+}

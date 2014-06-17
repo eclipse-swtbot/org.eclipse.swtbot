720b70058dd9557b08b18f6a3f386bf274e209e9
Xavier Raynaud <xavier.raynaud@kalray.eu>
2014-06-17 15:30:27 +0200

    bug 437588 (Add a "WaitForJob" Condition)
    Change-Id: Ic3c53b8227656578c6d88f29d9da747a95fb77a2
    Signed-off-by: Xavier Raynaud <xavier.raynaud@kalray.eu>

--- org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/waits/Conditions.java
+++ org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/waits/Conditions.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Ketan Padegaonkar and others.
+ * Copyright (c) 2008, 2014 Ketan Padegaonkar and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,9 +7,11 @@
  * 
  * Contributors:
  *     Ketan Padegaonkar - initial API and implementation
+ *     Xavier Raynaud <xavier.raynaud@kalray.eu> - bug 437588
  *******************************************************************************/
 package org.eclipse.swtbot.eclipse.finder.waits;
 
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IViewReference;
 import org.hamcrest.Matcher;
@@ -34,6 +36,15 @@ public class Conditions extends org.eclipse.swtbot.swt.finder.waits.Conditions {
 	 */
 	public static WaitForEditor waitForEditor(Matcher<IEditorReference> matcher) {
 		return new WaitForEditor(matcher);
+	}
+
+	/**
+	 * @param a job family (see {@link Job#belongsTo(Object)})
+	 * @param a human readable job family (used only for error message, may be null)
+	 * @return a condition that waits until all jobs of the given family are done.
+	 */
+	public static WaitForJobs waitForJobs(Object jobFamily, String humanReadableJobFamily) {
+		return new WaitForJobs(jobFamily, humanReadableJobFamily);
 	}
 
 }
---
+++ org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/waits/WaitForJobs.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Kalray and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Xavier Raynaud <xavier.raynaud@kalray.eu> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtbot.eclipse.finder.waits;
+
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.waits.ICondition;
+
+/**
+ * Waits until all {@link Job}s of the given family are finished.
+ * 
+ * @author Xavier Raynaud <xavier.raynaud@kalay.eu>
+ */
+public class WaitForJobs implements ICondition {
+
+    private final Object mFamily;
+	private final String mHumanReadableJobFamily;
+    
+	/**
+	 * Creates a condition that waits until all jobs of the given family are done.
+	 * 
+	 * @param family a job family.
+	 * @param humanReadableJobFamily a human readable name for the job family.
+	 *   It may be null, it serves only for the failure message.
+	 */
+    WaitForJobs(Object family, String humanReadableJobFamily) {
+        this.mFamily = family;
+        this.mHumanReadableJobFamily = humanReadableJobFamily;
+    }
+    
+    public boolean test() throws Exception {
+        Job[] allJobs = Job.getJobManager().find(mFamily);
+        return allJobs.length == 0;
+    }
+    
+    public void init(SWTBot bot) {
+    }
+
+    public String getFailureMessage() {
+    	String errMsg = "Wait for jobs failed: ";
+    	if (mHumanReadableJobFamily != null) {
+    		return mHumanReadableJobFamily + " jobs are still running.";
+    	}
+        return errMsg;
+    }
+
+}

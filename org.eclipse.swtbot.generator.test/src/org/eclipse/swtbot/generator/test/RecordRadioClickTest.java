874eeaaefa7cd066abdc02c7a598b58054d19d88
Aparna Argade <aprsac@yahoo.com>
2019-09-05 13:35:59 +0530

    Bug 550789: RadioButtonClickedRule not working
    -The condition for the rule checks for SWT.RADIO style. Also it checks
    getSelection() to avoid deselection event of other radio buttons.
    - buttonText is compared with empty string, instead of null, to match
    with Button.getText() return value.
    
    Change-Id: Ie5e8b7f3e9b60ab5427efdffb9e74731edee06df
    Signed-off-by: Aparna Argade <aprsac@yahoo.com>

---
+++ org.eclipse.swtbot.generator.test/src/org/eclipse/swtbot/generator/test/RecordRadioClickTest.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Cadence Design Systems, Inc..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Aparna Argade - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtbot.generator.test;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RecordRadioClickTest extends AbstractGeneratorTest {
+
+	@Override
+	protected void contributeToDialog(Composite container) {
+		Group group = new Group(container, SWT.NONE);
+		group.setLayout(new RowLayout(SWT.HORIZONTAL));
+		Button radio1 = new Button(group, SWT.RADIO);
+		radio1.setText("One"); //$NON-NLS-1$
+		Button radio2 = new Button(group, SWT.RADIO);
+		radio2.setText("Two"); //$NON-NLS-1$
+	}
+
+	@Test
+	public void testRadioButtonClick() {
+		this.bot.radio("Two").click(); // $NON-NLS-1$
+		flushEvents();
+		Assert.assertEquals("bot.radio(\"Two\").click();", //$NON-NLS-1$
+				recorderShellBot().text().getText().trim());
+	}
+}
--- org.eclipse.swtbot.generator/src/org/eclipse/swtbot/generator/framework/rules/simple/RadioButtonClickedRule.java
+++ org.eclipse.swtbot.generator/src/org/eclipse/swtbot/generator/framework/rules/simple/RadioButtonClickedRule.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Red Hat Inc..
+ * Copyright (c) 2012, 2019 Red Hat Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Mickael Istria (Red Hat) - initial API and implementation
+ *    Aparna Argade - Bug 550789
  *******************************************************************************/
 package org.eclipse.swtbot.generator.framework.rules.simple;
 
@@ -28,15 +29,15 @@ public class RadioButtonClickedRule extends GenerationSimpleRule {
 	@Override
 	public boolean appliesTo(Event event) {
 		return event.widget instanceof Button &&
-				(((Button)event.widget).getStyle() & SWT.CHECK) != 0
-				&& event.type == SWT.Selection;
+				(((Button)event.widget).getStyle() & SWT.RADIO) != 0
+				&& event.type == SWT.Selection && ((Button) event.widget).getSelection();
 	}
 
 	@Override
 	public void initializeForEvent(Event event) {
 		this.button = (Button)event.widget;
 		this.buttonText = this.button.getText();
-		if (this.buttonText != null) {
+		if (!this.buttonText.equals("")) {
 			this.buttonText = this.buttonText.replace("&", "");
 		} else {
 			this.index = WidgetUtils.getIndex((Button)event.widget);
@@ -48,10 +49,10 @@ public class RadioButtonClickedRule extends GenerationSimpleRule {
 		List<String> actions = new ArrayList<String>();
 
 		StringBuilder code = new StringBuilder();
-		if (this.buttonText != null) {
-			code.append("bot.checkBox(\"" + this.buttonText + "\")");
+		if (!this.buttonText.equals("")) {
+			code.append("bot.radio(\"" + this.buttonText + "\")");
 		} else {
-			code.append("bot.checkBox(" + this.index + ")");
+			code.append("bot.radio(" + this.index + ")");
 		}
 
 		code.append(".click()");
